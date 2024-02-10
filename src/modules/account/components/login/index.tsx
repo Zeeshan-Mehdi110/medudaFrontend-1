77b95cf3601f588df1bb1566d356d5c876d81eab
Oryan Tayer <oryantayer@gmail.com>
2024-02-10 19:50:06 +0200

    req access token

--- src/modules/account/components/login/index.tsx
+++ src/modules/account/components/login/index.tsx
@@ -43,7 +43,7 @@ const Login = ({ setCurrentView }: Props) => {
       <span className="text-center w-full text-lg mt-4">Or</span>
       <a
         type="button"
-        href="https://backend.pixelsjourney.com/store/auth/google"
+        href="https://backend.pixelsjourney.com/store/auth/google?requestAccessToken=true"
         className="text-white w-full justify-center ml-0 mr-0 mt-4 bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mb-2"
       >
         <svg
