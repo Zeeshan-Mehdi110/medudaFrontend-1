1c433a6846b45e28221b81ec8282e53aaed06586
Oryan Tayer <oryantayer@gmail.com>
2024-02-22 18:37:36 +0200

    cancel center slides

--- src/modules/home/components/hero-products/products/index.tsx
+++ src/modules/home/components/hero-products/products/index.tsx
@@ -74,8 +74,6 @@ const ProductSlider: React.FC<ProductSliderProps> = ({ products }) => {
        modules={[Navigation, Autoplay,Pagination, Scrollbar, A11y]}
        slidesPerView={'auto'}
        autoHeight={true}
-       
-      centeredSlides={true}
       spaceBetween={15}
       autoplay={{
         delay: 2200, // Delay in milliseconds between slides auto change, adjust as needed
