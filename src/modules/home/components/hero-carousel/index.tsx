74ecdcf997d021847c88aae1608bc0e7d3370816
Oryan Tayer <oryantayer@gmail.com>
2024-02-25 19:58:52 +0200

    add hero banner carousel

---
+++ src/modules/home/components/hero-carousel/index.tsx
@@ -0,0 +1,63 @@
+"use client"
+import { Swiper, SwiperSlide } from "swiper/react"
+import {
+  A11y,
+  Autoplay,
+  Navigation,
+  Pagination,
+  Scrollbar,
+} from "swiper/modules"
+import "swiper/css" // core Swiper
+import "swiper/css/navigation" // Navigation module
+import "swiper/css/pagination" // Pagination module
+import heroBanner from "../../../../../public/PixelsJourney.png"
+import heroBanner2 from "../../../../../public/PixelsJourneyCopy.png"
+import heroBanner3 from "../../../../../public/PixelsJourneyCopy2.png"
+
+const MySwiperComponent = () => {
+  return (
+    <Swiper
+      style={{ height: "100%", width: "100%" }}
+      modules={[Navigation,Autoplay, Pagination]}
+      spaceBetween={50}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: true,
+        pauseOnMouseEnter: true,
+        reverseDirection: true,
+      }}
+      loop={true}
+    >
+      <SwiperSlide>
+        {" "}
+        <img
+          src={heroBanner.src}
+          alt="Background"
+          className="absolute w-full h-full z-0 rounded-lg object-fit"
+        />{" "}
+      </SwiperSlide>
+      <SwiperSlide>
+        {" "}
+        <img
+          src={heroBanner2.src}
+          alt="Background"
+          className="absolute w-full h-full z-0 rounded-lg object-fit"
+        />{" "}
+      </SwiperSlide>
+      <SwiperSlide>
+        {" "}
+        <img
+          src={heroBanner3.src}
+          alt="Background"
+          className="absolute w-full h-full z-0 rounded-lg object-fit"
+        />{" "}
+      </SwiperSlide>
+      {/* Add more <SwiperSlide> components as needed */}
+    </Swiper>
+  )
+}
+
+export default MySwiperComponent
--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -2,13 +2,16 @@ import { Github } from "@medusajs/icons";
 import { Button, Heading } from "@medusajs/ui";
 import HeroProducts from "../hero-products";
 import heroBanner from "../../../../../public/PixelsJourney.png";
+import MySwiperComponent from "../hero-carousel";
 const Hero = () => {
   return (
     <div className="flex flex-col md:flex-row justify-center h-[75vh] gap-4 pr-2 pl-2 mt-4">
-      <div className="h-full w-full border-b border-ui-border-base relative bg-ui-bg-subtle flex justify-center rounded-lg shadow-xl" >
-      <img src={heroBanner.src} alt="Background" className="absolute w-full h-full z-0 rounded-lg object-fit" /> 
-        <div className="relative w-3/4 inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
-          {/* <span className="z-10 relative">
+      <div className="h-full flex-grow flex-shrink w-full md:w-4/5 border-b border-ui-border-base relative bg-ui-bg-subtle flex justify-center rounded-lg shadow-xl" >
+       {/* <img src={heroBanner.src} alt="Background" className="absolute w-full h-full z-0 rounded-lg object-fit" />  */}
+      <MySwiperComponent/>
+        {/* <div className="relative w-3/4 inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
+       
+           <span className="z-10 relative">
             <Heading level="h1" className="text-3xl leading-10 text-ui-fg-base font-normal">
               Pixels Journey
             </Heading>
@@ -21,10 +24,10 @@ const Hero = () => {
                 <Github />
               </Button>
             </a>
-          </span> */}
-        </div>
+          </span> 
+        </div> */}
       </div>
-      <div className="w-full sm:w-min pl-4 h-full mr-5 flex overflow-hidden flex-col float-right gap-2 shadow-xl rounded-lg">
+      <div className="w-full sm:w-auto pl-4 h-full mr-5 flex overflow-hidden flex-col float-right gap-2 shadow-xl rounded-lg">
           <HeroProducts />
         </div>
     </div>
