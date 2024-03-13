b10fdd057c6c73973cd2fc68e5d6f177ad2f4cb7
Anouar Kacem <kcm.anouar@gmail.com>
2024-03-13 13:33:13 +0100

    dynamic_server_usage fix

--- src/app/[countryCode]/layout.tsx
+++ src/app/[countryCode]/layout.tsx
@@ -1,4 +1,6 @@
 import { Metadata } from "next"
+export const dynamic = "force-dynamic"
+
 import "styles/globals.css"
 // import { Next, useMessages } from 'next-intl';
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "https://localhost:8000"
@@ -7,19 +9,17 @@ export const metadata: Metadata = {
   metadataBase: new URL(BASE_URL),
 }
 
-export default function RootLayout(props: { children: React.ReactNode}) {
+export default function RootLayout(props: { children: React.ReactNode }) {
   // const { params } = props;
   // const messages = useMessages();
   return (
-  
     // <html lang="en" data-mode="light">
-      
+
     //   <body className="dark:bg-black">
     //     <main className="relative ">{props.children}</main>
     //   </body>
-  
-    // </html>
-   <>{props.children}</>
 
+    // </html>
+    <>{props.children}</>
   )
 }
