dedeaad6130307805b86ba64905ffd947bf47c29
Oryan Tayer <oryantayer@gmail.com>
2024-03-13 18:00:02 +0200

    yet another fix to lang toggle

--- src/modules/layout/components/language-toggle/index.tsx
+++ src/modules/layout/components/language-toggle/index.tsx
@@ -16,11 +16,15 @@ const getCookie = (name: string): string | null => {
 }
 
 export default function LanguageToggle() {
-  const [lang, setLang] = useState(getCookie("lang") ?? localStorage.getItem("lang") ?? "en")
-
+  
+  const locales = ["en", "he", "ru"];
   const pathname = usePathname()
+  const pathnameParts = pathname
+  .split("/")
+  .filter((part) => part.trim() !== "")
+  const [lang, setLang] = useState(locales.includes(pathnameParts[1]) ? pathnameParts[1] : null ?? getCookie("lang") ?? localStorage.getItem("lang") ?? "en")
   const router = useRouter()
-
+  
   useEffect(() => {
     // setLang(getCookie("lang") || localStorage.getItem("lang") || "en")
     setLang(getCookie("lang") ?? localStorage.getItem("lang") ?? "en")
@@ -28,9 +32,7 @@ export default function LanguageToggle() {
 
   // Pre-calculate URLs for language change links
   const createLocaleChangeUrl = (newLocale: any) => {
-    const pathnameParts = pathname
-      .split("/")
-      .filter((part) => part.trim() !== "")
+  
     pathnameParts[1] = newLocale // Assuming the second segment is the locale
     return `/${pathnameParts.join("/")}`
   }
@@ -50,39 +52,6 @@ export default function LanguageToggle() {
   }, [lang])
 
   return (
-    // <div className="language-selector hover:text-ui-fg-base relative">
-    //   <button onClick={() => setIsOpen(!isOpen)} className="dropdown-button">
-    //     Change Language
-    //   </button>
-    //   {isOpen && (
-    //     <div className="dropdown-menu absolute right-0 mt-2 py-2 w-48 bg-white rounded-md shadow-xl z-20">
-    //       <span
-    //         className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer"
-    //         onClick={() => handleLanguageChange("en")}
-    //       >
-    //         English
-    //       </span>
-    //       <span
-    //         className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer"
-    //         onClick={() => handleLanguageChange("he")}
-    //       >
-    //         עברית
-    //       </span>
-    //       <span
-    //         className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer"
-    //         onClick={() => handleLanguageChange("ru")}
-    //       >
-    //         Русский
-    //       </span>
-    //     </div>
-    //   )}
-    //   <style jsx>{`
-    //     .dropdown-menu {
-    //       display: ${isOpen ? "block" : "none"};
-    //     }
-    //   `}</style>
-    // </div>
-
     <div className="language-selector hover:text-ui-fg-base">
     <select className=" dark:text-white bg-transparent dark:bg-black" onChange={(e) => {
                                                                                 handleLanguageChange(e.target.value);
