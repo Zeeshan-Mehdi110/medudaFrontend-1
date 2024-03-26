fb7423b69d123d8cd0c884d8344f504c61ca7b13
Oryan Tayer <oryantayer@gmail.com>
2024-03-26 20:54:35 +0200

    .

--- src/app/[countryCode]/[locale]/(main)/customer-service/page.tsx
+++ src/app/[countryCode]/[locale]/(main)/customer-service/page.tsx
@@ -8,7 +8,7 @@ export default async function CustomerService({params}: {params: any}) {
   const {locale} = params;
   const { t } = await initTranslations(locale, ['common']);
   return (
-    <div className="flex flex-col min-h-screen py-2 dark:text-white">
+    <div className="flex flex-col py-6 dark:text-white">
        <h1 className="text-2xl-semi font-bold text-gray-900 text-center dark:text-white mt-10">
           {t("customer-service")}
         </h1>
