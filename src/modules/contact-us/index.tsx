e24e230fa907e1d057e468dc870a2090d7d9a3ca
Oryan Tayer <oryantayer@gmail.com>
2024-03-26 16:19:43 +0200

    .

--- src/modules/contact-us/index.tsx
+++ src/modules/contact-us/index.tsx
@@ -48,15 +48,15 @@ const ContactUs = () => {
       <form onSubmit={sendEmail} className="space-y-4">
         <div>
           <label htmlFor="name" className="block dark:text-white text-sm font-medium text-gray-700">{t('name')}</label>
-          <input type="text" name="name" id="name" required className="mt-1 block w-full rounded-md border-2 h-8 border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"/>
+          <input type="text" name="name" id="name" required className="mt-1 block w-full rounded-md border-2 text-gray-900 h-8 border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"/>
         </div>
         <div>
           <label htmlFor="email" className="block  dark:text-white text-sm font-medium text-gray-700">{t('email')}</label>
-          <input type="email" name="email" id="email" required className="mt-1 block w-full border-2 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 h-8 sm:text-sm"/>
+          <input type="email" name="email" id="email" required className="mt-1 block w-full border-2 rounded-md text-gray-900 border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 h-8 sm:text-sm"/>
         </div>
         <div>
           <label htmlFor="message" className="block dark:text-white text-sm font-medium text-gray-700">{t('contact-us-message')}</label>
-          <textarea name="message" id="message" rows={3} required className="mt-1 block w-full border-2 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"></textarea>
+          <textarea name="message" id="message" rows={3} required className="mt-1 block w-full text-gray-900 border-2 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"></textarea>
         </div>
         {/* <button type="submit" className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
         
