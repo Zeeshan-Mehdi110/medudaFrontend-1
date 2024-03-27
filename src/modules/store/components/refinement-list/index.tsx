c41dd7f8b902ccec50d62bc88c5df197b4122b12
Oryan Tayer <oryantayer@gmail.com>
2024-03-27 21:45:03 +0200

    padding left

--- src/modules/store/components/refinement-list/index.tsx
+++ src/modules/store/components/refinement-list/index.tsx
@@ -88,7 +88,7 @@ const RefinementList = ({ sortBy }: RefinementListProps) => {
       <div className="flex small:flex-col gap-12 py-4 mb-8 small:px-0 pl-6 small:min-w-[250px] small:ml-[1.675rem]">
         <SortProducts sortBy={sortBy} setQueryParams={setQueryParams} />
       </div>
-      <div className="flex flex-row justify-between md:flex md:flex-col md:justify-normal">
+      <div className="flex flex-row justify-between md:flex md:flex-col md:justify-normal pl-6">
       {categories && (
         <>
           {/* <div >
