11ef7b6a1777625137fce495f2f39872964d2553
Oryan Tayer <oryantayer@gmail.com>
2024-04-05 15:23:37 +0300

    adding not found page to [locale] directory

---
+++ src/app/[countryCode]/[locale]/not-found.tsx
@@ -0,0 +1,23 @@
+import { Metadata } from "next"
+
+import InteractiveLink from "@modules/common/components/interactive-link"
+import initTranslations from 'app/i18n';
+import { getLang } from '@lib/data';
+export const metadata: Metadata = {
+  title: "404",
+  description: "Something went wrong",
+}
+
+export default async function NotFound(props: { children: React.ReactNode, params: { locale: string }}) {
+  const { params } = props;
+  const { t } = await initTranslations(params.locale, ['common']);
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center min-h-[calc(100vh-64px)]">
+      <h1 className="text-2xl-semi text-ui-fg-base">{t("page-not-found")}</h1>
+      <p className="text-small-regular text-ui-fg-base">
+      {t("page-not-found-info")}
+      </p>
+      <InteractiveLink href="/">{t("go-to-homepage")}</InteractiveLink>
+    </div>
+  )
+}
