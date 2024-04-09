a736afc441cae2a83004cd797288f282b425f484
Oryan Tayer <oryantayer@gmail.com>
2024-04-09 20:45:11 +0300

    .

--- src/modules/layout/components/language-toggle/index.tsx
+++ src/modules/layout/components/language-toggle/index.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useState, useEffect } from "react"
 import { usePathname, useRouter } from "next/navigation" // Correct import for useRouter
-
+import { useTranslation } from "react-i18next"
 const setCookie = (name: string, value: string, days: number) => {
   const expires = new Date(Date.now() + days * 864e5).toUTCString()
   document.cookie =
@@ -18,6 +18,7 @@ const getCookie = (name: string): string | null => {
 export default function LanguageToggle({locale} : {locale: string}) {
   
   const locales = ["en", "he", "ru"];
+  const { t } = useTranslation()
   const pathname = usePathname()
   const pathnameParts = pathname
   .split("/")
@@ -26,8 +27,6 @@ export default function LanguageToggle({locale} : {locale: string}) {
   const router = useRouter()
   
   useEffect(() => {
-    // setLang(getCookie("lang") || localStorage.getItem("lang") || "en")
-    // setLang(locales.includes(pathnameParts[1]) ? pathnameParts[1] : null ?? getCookie("lang") ?? localStorage.getItem("lang") ?? "en")
     localStorage.setItem("lang", lang)
     setCookie("lang", lang, 365)
     setCookie("NEXT_LOCALE", lang, 365)
@@ -36,27 +35,21 @@ export default function LanguageToggle({locale} : {locale: string}) {
   // Pre-calculate URLs for language change links
   const createLocaleChangeUrl = (newLocale: any) => {
   
-    pathnameParts[1] = newLocale // Assuming the second segment is the locale
+    pathnameParts[1] = newLocale 
     return `/${pathnameParts.join("/")}`
   }
 
   const handleLanguageChange = (newLocale: any) => {
     setLang(newLocale)
-    // setCookie("lang", newLocale, 365)
-    // setCookie("NEXT_LOCALE", newLocale, 365)
-    // setIsOpen(false)
-    // No router.push here since <Link> handles navigation
     router.replace(createLocaleChangeUrl(newLocale))
   }
 
-  // useEffect(() => {
-  //   // Effect for updating state, cookies, or localStorage when lang changes might be placed here
-  //   localStorage.setItem("lang", lang)
-  // }, [lang])
+
 
   return (
     <div className="language-selector hover:text-ui-fg-base">
-    <select className=" dark:text-white bg-transparent dark:bg-black" onChange={(e) => {
+      <label htmlFor="language-select" className="sr-only">{t("choose-language")}</label>
+    <select id="language-select" className=" dark:text-white bg-transparent dark:bg-black" onChange={(e) => {
                                                                                 handleLanguageChange(e.target.value);
                                                                                 
                                                                               }} value={lang}>
