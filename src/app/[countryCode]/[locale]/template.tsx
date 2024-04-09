6ade0a3059c109c49001641587a3d087a69c46cf
Oryan Tayer <oryantayer@gmail.com>
2024-04-09 11:34:29 +0300

    .

--- src/app/[countryCode]/[locale]/template.tsx
+++ src/app/[countryCode]/[locale]/template.tsx
@@ -5,9 +5,9 @@ import {motion} from "framer-motion";
 export default function Template({children} : {children: React.ReactNode}) {
   return (
     <motion.div
-      initial={{y:20, opacity: 0 }}
+      initial={{y:15, opacity: 0 }}
       animate={{y:0, opacity: 1 }}
-      transition={{ease:'easeIn', duration: 0.8}}
+      transition={{ease:'easeInOut', duration: 0.8}}
     >
      {children}
     </motion.div>
