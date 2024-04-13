f478a0f45de333a22027819790c342d5759f8fd4
Oryan Tayer <oryantayer@gmail.com>
2024-04-13 18:45:32 +0300

    adding filteration data to posts

--- src/modules/blog/index.tsx
+++ src/modules/blog/index.tsx
@@ -68,9 +68,9 @@ const BlogModule: React.FC<{locale:string}> = ({locale}:{locale:string}) => {
   }, [locale]);
 
   return (
-    <div className="flex flex-col w-full pr-6 pl-6 gap-4 justify-center items-center">
+    <section aria-label={t("blog-section")} className="flex flex-col w-full pr-6 pl-6 gap-4 justify-center items-center">
       {blogPosts.map((post) => (
-        <a key={post.attributes.uniqueId} href={`/blog/${post.attributes.uniqueId}`} className="blog-post-card sm:w-3/4 w-full mb-4 p-4 rounded border dark:text-white border-gray-200 shadow-lg ">
+        <a key={post.attributes.uniqueId} aria-label={t("blog-post-link")}  data-date={convertToDDMMYYYY(post.attributes.publishedAt)} data-author={post.attributes.Author} data-tags={post.attributes[`tags_${locale}` as keyof BlogPostAttributes]} href={`/blog/${post.attributes.uniqueId}`} className="blog-post-card sm:w-3/4 w-full mb-4 p-4 rounded border dark:text-white border-gray-200 shadow-lg ">
         <div className="h-[300px] mb-4 fade-bottom-text ">
           <h2 className="mb-2 text-lg font-semibold">{post.attributes.Author}</h2>
           <div className="flex justify-center text-xl mb-2">
@@ -82,11 +82,11 @@ const BlogModule: React.FC<{locale:string}> = ({locale}:{locale:string}) => {
          <div>
         
       </div>
-         <a  href={`/blog/${post.attributes.uniqueId}`} className="text-blue-500 p-4 hover:underline"
+         <a  href={`/blog/${post.attributes.uniqueId}`} aria-label={t("blog-post-link")}  className="text-blue-500 p-4 hover:underline"
             >{t("read-more")}</a>
          </a >
       ))}
-    </div>
+    </section>
   );
 };
 
