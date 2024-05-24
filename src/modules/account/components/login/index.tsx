e7170a1a18a56bfb4965b6bc69af2d3cf2ad8896
Oryan Tayer <oryantayer@gmail.com>
2024-05-24 13:40:26 +0300

    add margin left in google auth button

--- src/modules/account/components/login/index.tsx
+++ src/modules/account/components/login/index.tsx
@@ -49,7 +49,7 @@ const {t} = useTranslation()
         className="text-white w-full justify-center ml-0 mr-0 mt-4 bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mb-2"
       >
         <svg
-          className="mr-2 -ml-1 w-4 h-4"
+          className="mr-2 ml-2 w-4 h-4"
           aria-hidden="true"
           focusable="false"
           data-prefix="fab"
