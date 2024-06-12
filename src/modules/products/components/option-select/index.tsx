1a02f9714d49dd01b2ce49b3323f81d9a4f58092
Oryan Tayer <oryantayer@gmail.com>
2024-06-12 18:15:12 +0300

    .

--- src/modules/products/components/option-select/index.tsx
+++ src/modules/products/components/option-select/index.tsx
@@ -35,7 +35,7 @@ const OptionSelect: React.FC<OptionSelectProps> = ({
    //@ts-ignore
    const options = productVariants.map(pv => pv.options[0]);
    option.values = options;
-  const filteredOptions = option.values.map((v) => v).filter(onlyUnique)
+  const filteredOptions = option.values.map((v) => v.value).filter(onlyUnique)
  const { t } = useTranslation()
   return (
     <div className="flex flex-col gap-y-3">
@@ -44,18 +44,18 @@ const OptionSelect: React.FC<OptionSelectProps> = ({
         {filteredOptions.map((v) => {
           return (
             <button
-              onClick={() => updateOption({ [option.id]: v.value })}
-              key={v.value}
+              onClick={() => updateOption({ [option.id]: v })}
+              key={v}
               className={clx(
                 "border-ui-border-base bg-ui-bg-subtle dark:text-white border text-small-regular h-10 rounded-rounded p-2 flex-1 ",
                 {
-                  "border-ui-border-interactive": v.value === current,
+                  "border-ui-border-interactive": v === current,
                   "hover:shadow-elevation-card-rest transition-shadow ease-in-out duration-150":
-                    v.value !== current,
+                    v !== current,
                 }
               )}
             >
-              <TextConvertor locale={locale} title={v.value} metadata={v.metadata && v.metadata["name"] ? JSON.stringify(v.metadata["name"]) as any : null} ></TextConvertor>
+              <TextConvertor locale={locale} title={v} metadata={ options.find((o) => o.value === v)?.metadata && options.find((o) => o.value === v)?.metadata["name"] ? JSON.stringify(options.find((o) => o.value === v)?.metadata["name"]) as any : null} ></TextConvertor>
             </button>
           )
         })}
