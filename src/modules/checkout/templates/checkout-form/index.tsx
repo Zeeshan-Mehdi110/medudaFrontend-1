996de236401d05312f971de78cd64717c49b6c47
Oryan Tayer <oryantayer@gmail.com>
2024-06-12 17:08:29 +0300

    skip shipping if every item in the cart is gift card

--- src/modules/checkout/templates/checkout-form/index.tsx
+++ src/modules/checkout/templates/checkout-form/index.tsx
@@ -1,3 +1,72 @@
+// import Addresses from "@modules/checkout/components/addresses"
+// import Shipping from "@modules/checkout/components/shipping"
+// import Payment from "@modules/checkout/components/payment"
+// import Review from "@modules/checkout/components/review"
+// import {
+//   createPaymentSessions,
+//   getCustomer,
+//   listShippingMethods,
+// } from "@lib/data"
+// import { cookies } from "next/headers"
+// import { CartWithCheckoutStep } from "types/global"
+// import { getCheckoutStep } from "@lib/util/get-checkout-step"
+
+// export default async function CheckoutForm({locale}: {locale: string}) {
+//   const cartId = cookies().get("_medusa_cart_id")?.value
+//   if (!cartId) {
+//     return null
+//   }
+
+//   // create payment sessions and get cart
+//   const cart = (await createPaymentSessions(cartId).then(
+//     (cart) => cart
+//   )) as CartWithCheckoutStep
+
+//   if (!cart) {
+//     return null
+//   }
+
+//   cart.checkout_step = cart && getCheckoutStep(cart)
+
+//   // get available shipping methods
+//   const availableShippingMethods = await listShippingMethods(
+//     cart.region_id
+//   ).then((methods) => methods?.filter((m) => !m.is_return))
+
+//   if (!availableShippingMethods) {
+//     return null
+//   }
+
+//   // get customer if logged in
+//   const customer = await getCustomer()
+
+//   return (
+//     <div>
+//       <div className="w-full grid grid-cols-1 gap-y-8 bg-white  dark:text-white dark:bg-black">
+//         <div>
+//           <Addresses locale={locale} cart={cart} customer={customer} />
+//         </div>
+
+//         <div>
+//           <Shipping
+//             cart={cart}
+//             availableShippingMethods={availableShippingMethods}
+//           />
+//         </div>
+
+//         <div>
+//           <Payment locale={locale} cart={cart} />
+//         </div>
+
+//         <div>
+//           <Review cart={cart} />
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+
 import Addresses from "@modules/checkout/components/addresses"
 import Shipping from "@modules/checkout/components/shipping"
 import Payment from "@modules/checkout/components/payment"
@@ -11,7 +80,7 @@ import { cookies } from "next/headers"
 import { CartWithCheckoutStep } from "types/global"
 import { getCheckoutStep } from "@lib/util/get-checkout-step"
 
-export default async function CheckoutForm({locale}: {locale: string}) {
+export default async function CheckoutForm({ locale }: { locale: string }) {
   const cartId = cookies().get("_medusa_cart_id")?.value
   if (!cartId) {
     return null
@@ -27,7 +96,7 @@ export default async function CheckoutForm({locale}: {locale: string}) {
   }
 
   cart.checkout_step = cart && getCheckoutStep(cart)
-
+  const isOnlyGiftCard = cart.items.every((item) => item.is_giftcard)
   // get available shipping methods
   const availableShippingMethods = await listShippingMethods(
     cart.region_id
@@ -46,13 +115,14 @@ export default async function CheckoutForm({locale}: {locale: string}) {
         <div>
           <Addresses locale={locale} cart={cart} customer={customer} />
         </div>
-
-        <div>
-          <Shipping
-            cart={cart}
-            availableShippingMethods={availableShippingMethods}
-          />
-        </div>
+        {!isOnlyGiftCard && (
+          <div>
+            <Shipping
+              cart={cart}
+              availableShippingMethods={availableShippingMethods}
+            />
+          </div>
+        )}
 
         <div>
           <Payment locale={locale} cart={cart} />
