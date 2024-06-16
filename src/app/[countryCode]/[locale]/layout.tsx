681c963e805bb24c0ad6a19b761a15b286f0cb69
Oryan Tayer <oryantayer@gmail.com>
2024-06-16 21:20:15 +0300

    static params for 'countryCode/locale'

--- src/app/[countryCode]/[locale]/layout.tsx
+++ src/app/[countryCode]/[locale]/layout.tsx
@@ -3,9 +3,43 @@ import "styles/globals.css"
 import TranslationsProvider from "../../../modules/translations/TranslationsProvider";
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "https://localhost:8000"
 import initTranslations from '../../i18n'
-export const metadata: Metadata = {
-  metadataBase: new URL(BASE_URL),
+import { locales } from "../../../../config/config";
+import { listRegions } from "@lib/data";
+
+type Props = {
+  params: {countryCode: string; locale: string }
 }
+
+
+export async function generateStaticParams() {
+  const countryCodes = await listRegions().then((regions) =>
+    regions?.map((r) => r.countries.map((c) => c.iso_2)).flat()
+  );
+
+  let staticParams: any = [];
+
+  locales.forEach((locale) => {
+    const localeParams = countryCodes
+      ?.map((countryCode) => ({
+        countryCode,
+        locale
+      }))
+      .flat() ?? [];
+
+    staticParams = [...staticParams, ...localeParams];
+  });
+  return staticParams;
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  
+  
+  return {
+    title: "Pixels Jourey Store",
+    description: "Pixels Jourey Store",
+  }
+}
+
 const i18NameSpaces = ['common','profile',"en"];
 export default async function Layout(props: { children: React.ReactNode, params: { locale: string }}) {
   const { params:{locale} } = props;
