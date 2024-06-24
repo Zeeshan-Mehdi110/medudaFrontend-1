e32e2620e95bf523c51b0fb4f10a624d430a90ee
Oryan Tayer <oryantayer@gmail.com>
2024-06-24 18:59:10 +0300

    .

--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -8,7 +8,7 @@ import Theme from "@modules/layout/components/theme-toggle"
 import LanguageToggle from "@modules/layout/components/language-toggle"
 import initTranslations from "app/i18n"
 import Image from "next/image"
-import { ShoppingCart } from "@medusajs/icons"
+import { MagnifyingGlass, ShoppingCart, User } from "@medusajs/icons"
 import { Merienda } from 'next/font/google'
 import PromotionText from "@modules/home/components/promotion-text"
 const merienda = Merienda({
@@ -24,6 +24,7 @@ export default async function Nav(params:any) {
 
   return (
     <div className="sticky top-0 inset-x-0 z-50 group ">
+       <PromotionText></PromotionText>
       <header className="relative  dark:bg-black bg-ui-bg-subtle h-16 mx-auto border-b duration-200  border-ui-border-base">
       <nav className="content-container overflow-x-hidden overflow-y-hidden txt-xsmall-plus text-ui-fg-subtle flex items-center justify-between w-full  h-full text-small-regular gap-[2px] pr-2 pl-2">
         <div className="flex-1 basis-0 h-full flex items-center sm:justify-between gap-4">
@@ -50,28 +51,31 @@ export default async function Nav(params:any) {
           <div className="flex items-center gap-1 scale-90 sm:scale-100 sm:gap-x-6 h-full flex-1 basis-0 justify-end sm:pl-0">
             <div className="hidden small:flex items-center gap-x-6 h-full">
               {process.env.FEATURE_SEARCH_ENABLED && (
-                <LocalizedClientLink
-                  className="hover:text-ui-fg-base"
-                  href="/search"
-                  scroll={false}
-                >
-                  {t("search")}
-                </LocalizedClientLink>
+             <LocalizedClientLink
+             className="hover:text-ui-fg-base flex flex-col justify-center items-center"
+             href="/search"
+             scroll={false}
+           >
+             <MagnifyingGlass/>
+             {t("search")}
+           </LocalizedClientLink>
               )}
-              <LocalizedClientLink
-                className="hover:text-ui-fg-base"
+           <LocalizedClientLink
+                className="hover:text-ui-fg-base flex flex-col justify-center items-center"
                 href="/account"
               >
-                 {t("account")}
+                <User/>
+                {t("account")}
               </LocalizedClientLink>
             </div>
             <Suspense
               fallback={
                 <a
-                  className="hover:text-ui-fg-base flex gap-2 ml "
+                  className="hover:text-ui-fg-base flex flex-col gap-2 ml "
                   href="/cart"
                 >
-                    <ShoppingCart className="mr-1" /> 
+                  <ShoppingCart className="mr-1" />
+                  {t("cart")}
                 </a>
               }
             >
@@ -84,7 +88,6 @@ export default async function Nav(params:any) {
           </div>
         </nav>
       </header>
-      <PromotionText></PromotionText>
     </div>
   )
 }
