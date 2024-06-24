09d83113c6973ae2530e3752e3e60f4ef009828c
Oryan Tayer <oryantayer@gmail.com>
2024-06-24 16:27:59 +0300

    .

--- src/modules/home/components/hero-carousel/index.tsx
+++ src/modules/home/components/hero-carousel/index.tsx
@@ -1,67 +1,3 @@
-// "use client"
-// import { Swiper, SwiperSlide } from "swiper/react"
-// import {
-//   A11y,
-//   Autoplay,
-//   Navigation,
-//   Pagination,
-//   Scrollbar,
-// } from "swiper/modules"
-// import "swiper/css" // core Swiper
-// import "swiper/css/navigation" // Navigation module
-// import "swiper/css/pagination" // Pagination module
-// import heroBanner from "../../../../../public/PixelsJourney.png"
-// import heroBanner2 from "../../../../../public/PixelsJourneyCopy.png"
-// import heroBanner3 from "../../../../../public/PixelsJourneyCopy2.png"
-// import './custom-swiper-style.css';
-// const MySwiperComponent = ({locale} : {locale:string}) => {
-//   return (
-//     <Swiper
-//       style={{ height: "100%", width: "100%" }}
-//       modules={[Navigation,Autoplay, Pagination]}
-//       spaceBetween={50}
-//       slidesPerView={1}
-//       navigation
-//       pagination={{ clickable: true }}
-//       autoplay={{
-//         delay: 2500,
-//         disableOnInteraction: true,
-//         pauseOnMouseEnter: true,
-//         reverseDirection: true,
-//       }}
-//       loop={true}
-      
-//     >
-//       <SwiperSlide>
-//         {" "}
-//         <img
-//           src={heroBanner.src}
-//           alt="Background"
-//           className="absolute w-full h-full z-0 rounded-lg object-fit"
-//         />{" "}
-//       </SwiperSlide>
-//       <SwiperSlide>
-//         {" "}
-//         <img
-//           src={heroBanner2.src}
-//           alt="Background"
-//           className="absolute w-full h-full z-0 rounded-lg object-fit"
-//         />{" "}
-//       </SwiperSlide>
-//       <SwiperSlide>
-//         {" "}
-//         <img
-//           src={heroBanner3.src}
-//           alt="Background"
-//           className="absolute w-full h-full z-0 rounded-lg object-fit"
-//         />{" "}
-//       </SwiperSlide>
-//       {/* Add more <SwiperSlide> components as needed */}
-//     </Swiper>
-//   )
-// }
-
-// export default MySwiperComponent
 
 "use client"
 import { Swiper, SwiperSlide } from "swiper/react"
@@ -195,7 +131,7 @@ const MySwiperComponent = ({ locale }: { locale: string }) => {
                       <div className="button-wrapper w-full flex justify-center items-center">
                         <Button
                           onClick={() => router.push(img.link)}
-                          variant="primary"
+                          className="custom-button"
                         >
                           {img.linkText}
                         </Button>
