25e3a3652e77aed1a7ee3d516f6597d5c6065589
Oryan Tayer <oryantayer@gmail.com>
2024-06-24 16:17:55 +0300

    .

--- src/modules/home/components/promotion-text/index.tsx
+++ src/modules/home/components/promotion-text/index.tsx
@@ -1,34 +1,47 @@
-"use client";
-import React from "react";
-import { usePathname } from "next/navigation";
-import { useParams } from "next/navigation";
-
+"use client"
+import React from "react"
+import { usePathname } from "next/navigation"
+import { useParams } from "next/navigation"
+import { useState } from "react"
+import { XCircle } from "@medusajs/icons"
 interface PromotionTextProps {
-  text?: string;
+  text?: string
 }
 
 const PromotionText: React.FC<PromotionTextProps> = ({ text }) => {
-  const path = usePathname();
-  const params = useParams();
-  const { locale, countryCode } = params || {}; // Ensure params is not undefined
-  const isHomePage = path === `/${countryCode}/${locale}`;
+  const path = usePathname()
+  const params = useParams()
+  const { locale, countryCode } = params || {} // Ensure params is not undefined
+  const isHomePage = path === `/${countryCode}/${locale}`
+  const [isPromotionTextVisible, setIsPromotionTextVisible] = useState(true)
 
-  console.log(path);
-  
+const handleClosePromotion = () => {
+    setIsPromotionTextVisible(false)
+}
+
   return (
     <>
-      {isHomePage && (
-        <div className="marquee-container bg-violet-700 mb-2">
-          <div className="marquee text-white">
-            <p>
-              Promotion 1: Get 20% off on all items! | Promotion 2: Free shipping on
-              orders over $50! | Promotion 3: Buy 2 get 1 free on select items!
-            </p>
+      {isHomePage && isPromotionTextVisible && (
+        <>
+          <div className="bg-violet-700 w-full flex items-center gap-3 rounded-sm">
+          <div className="w-fit"><a href="#" onClick={handleClosePromotion}><XCircle className="text-white"></XCircle></a></div>
+          
+          <div className="marquee-container w-max">
+          
+            <div className="marquee text-white">
+          
+              <p>
+                Promotion 1: Get 20% off on all items! | Promotion 2: Free
+                shipping on orders over $50! | Promotion 3: Buy 2 get 1 free on
+                select items!
+              </p>
+            </div>
           </div>
-        </div>
+          </div>
+        </>
       )}
     </>
-  );
-};
+  )
+}
 
-export default PromotionText;
+export default PromotionText
