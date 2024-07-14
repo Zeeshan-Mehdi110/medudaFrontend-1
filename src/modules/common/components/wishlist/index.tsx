f85a536c275c7600a7fa9fb41a77c8b59fc5c4d1
Oryan Tayer <oryantayer@gmail.com>
2024-07-14 11:19:31 +0300

    .

--- src/modules/common/components/wishlist/index.tsx
+++ src/modules/common/components/wishlist/index.tsx
@@ -55,7 +55,7 @@ const WishList: React.FC<WishListProps> = ({
   return (
     <div className="flex flex-col gap-3">
       <h2>Wish List</h2>
-      <div style={{ display: "flex", gap: "1rem" }}>
+      <div className="grid grid-cols-2 sm:grid-cols-4">
         {wishlist.map((item, index) => (
           <div className="w-[180px]"  key={item.id}>
             {" "}
